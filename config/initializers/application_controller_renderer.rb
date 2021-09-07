5de26760e041b26de3bac20b714dedaeae568035
ichi-a00 <prenehru@gmail.com>
2021-09-07 03:40:45 +0000

    [START]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '73f09d1cdd562cfad172eb8b43d460285594909430cb4e775a8dafd2d3b0983d2a3e0446b1151144b1933c218617b0c886eacceea4daee0761307e41b1d158c2'
